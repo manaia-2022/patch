70a0e3354ebe1a2faf2bd8ba3e04e8d0721595f6
edan-mourie <107600822+edanmourie@users.noreply.github.com>
2022-09-14 11:03:19 +1200

    started authentication server-side

---
+++ server/routes/pets.router.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const checkJwt = require('../auth0')
+const db = require('../db/functions/users')
+
+// TODO: use checkJwt as middleware
+// GET /api/v1/users
+router.get('/', checkJwt, (req, res) => {
+  const auth0_id = req.user?.sub
+
+  if (!auth0_id) {
+    res.send(null)
+  } else {
+    db.getUser(auth0_id)
+      .then((user) => {
+        res.json(user ? user : null)
+      })
+      .catch((err) => res.status(500).send(err.message))
+  }
+})
+
+module.exports = router
