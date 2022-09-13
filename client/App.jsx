ab93fbde29ac5dc684440f8c6d423caf70462bd0
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-09-14 11:24:26 +1200

    started client side auth

--- client/App.jsx
+++ client/App.jsx
@@ -1,27 +1,31 @@
 import { useState } from 'react'
 
 import MainLayout from './components/Layout/MainLayout'
+import Nav from './components/Nav.jsx'
 
 function App() {
   const [count, setCount] = useState(1)
   return (
-    <MainLayout>
-      <div className='mt-4'>
-        <div className='w-20 rounded-md border-2 border-violet-600 bg-violet-500 p-2 text-center shadow-md shadow-violet-300'>
-          <h1 className='text-mono font-bold tracking-wider text-violet-100'>
-            Patch
-          </h1>
-        </div>
+    <div>
+      <Nav />
+      <MainLayout>
         <div className='mt-4'>
-          <button
-            onClick={() => setCount((c) => c + 1)}
-            className='rounded-md border-2 border-slate-600 bg-slate-500 p-2 font-mono text-slate-100 shadow-md shadow-slate-300'
-          >
-            {count}: Increment
-          </button>
+          <div className='w-20 rounded-md border-2 border-violet-600 bg-violet-500 p-2 text-center shadow-md shadow-violet-300'>
+            <h1 className='text-mono font-bold tracking-wider text-violet-100'>
+              Patch
+            </h1>
+          </div>
+          <div className='mt-4'>
+            <button
+              onClick={() => setCount((c) => c + 1)}
+              className='rounded-md border-2 border-slate-600 bg-slate-500 p-2 font-mono text-slate-100 shadow-md shadow-slate-300'
+            >
+              {count}: Increment
+            </button>
+          </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </div>
   )
 }
 
---
+++ client/components/Nav.jsx
@@ -0,0 +1,48 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+
+function Nav() {
+  // TODO: call the useAuth0 hook and destructure logout and loginWithRedirect
+  const { user, logout, loginWithRedirect } = useAuth0()
+  const handleLogOff = (e) => {
+    e.preventDefault()
+    return logout()
+  }
+
+  const handleSignIn = (e) => {
+    e.preventDefault()
+    return loginWithRedirect()
+  }
+
+  console.log(user)
+
+  return (
+    <>
+      <div>
+        <Link to='/'>Home</Link>
+        <IfAuthenticated>
+          <Link to='/' onClick={handleLogOff}>
+            Log off
+          </Link>
+          <p>
+            <span role='img' alt={user}>
+              {user}
+            </span>
+            {' ' + user}
+          </p>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <Link to='/' onClick={handleSignIn}>
+            Sign In
+          </Link>
+        </IfNotAuthenticated>
+      </div>
+      <h1>Fruit FTW!</h1>
+    </>
+  )
+}
+
+export default Nav
