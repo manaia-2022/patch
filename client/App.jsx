1735308539dcc62ecd221ca7cddcf93cf4118147
Haunani Pao <90392354+haunanipao@users.noreply.github.com>
2022-09-14 17:42:23 +1200

    Done? All day we played with Tailwind CSS

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,16 @@
-import Card from './components/Layout/Card'
+import { Route, Routes } from 'react-router-dom'
+
+import DiscoverRoute from './components/DiscoverRoute'
 import MainLayout from './components/Layout/MainLayout'
 
 function App() {
   return (
     <MainLayout>
-      <Card />
+      <Routes>
+        <Route path='/' element={<DiscoverRoute />} />
+        <Route path='/my-pets' element={<div>My Pets Route</div>} />
+        <Route path='/my-pets/add' element={<div>Add Pet Route</div>} />
+      </Routes>
     </MainLayout>
   )
 }
