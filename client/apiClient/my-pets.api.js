5bab9790be594f5cad8b71ffe6cfd874f36e7a11
Patricia Tararo <107485900+trish-tararo-03@users.noreply.github.com>
2022-09-15 12:04:07 +1200

    added token parameter to getMyPets function

--- client/apiClient/my-pets.api.js
+++ client/apiClient/my-pets.api.js
@@ -2,8 +2,11 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1/my-pets'
 
-export function getMyPets() {
-  return request.get(rootUrl).then((res) => {
-    return res.body.pets
-  })
+export function getMyPets(token) {
+  return request
+    .get(rootUrl)
+    .set('Authorization', `Bearer ${token}`)
+    .then((res) => {
+      return res.body.pets
+    })
 }
