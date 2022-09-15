3a7a287de2bcf0b9b5bc1d962c3a0cf557397f3a
ash-fitzgibbon <107602601+ash-fitzgibbon@users.noreply.github.com>
2022-09-16 11:39:22 +1200

    page layout fixed

--- client/components/AddComment.jsx
+++ client/components/AddComment.jsx
@@ -48,25 +48,33 @@ export default function AddComment(props) {
     <>
       <div className='flex justify-center'>
         <form onSubmit={handleSubmit}>
-          <label htmlFor='content'>
-            Leave a comment for {props.pet.name}:{' '}
-          </label>
-          <input
-            className=' border-2 border-solid border-black pr-4'
-            type='text'
-            id='content'
-            name='content'
-            onChange={handleChange}
-            value={form.content}
-          ></input>
-          <button
-            className='rounded-full bg-purple-500 py-2 px-4 pl-4 font-bold text-white hover:bg-purple-700'
-            type='submit'
-          >
-            Submit
-          </button>
+          <div className='flex justify-center  p-10'>
+            <label htmlFor='content' className='text-2xl font-normal'>
+              Leave a comment for {props.pet.name}:{' '}
+            </label>
+          </div>
+          <div className='flex justify-center'>
+            <input
+              className=' flex justify-center border-2 border-solid border-black p-10 pr-4'
+              type='text'
+              id='content'
+              name='content'
+              onChange={handleChange}
+              value={form.content}
+            ></input>
+          </div>
+          <div className='flex justify-center p-10'>
+            <button
+              className='rounded-full bg-purple-700 p-10 py-2 px-4 pl-4 font-bold text-white  hover:bg-pink-700'
+              type='submit'
+            >
+              Add Comment
+            </button>
+          </div>
+          <div className='flex justify-center text-2xl font-normal'>
+            {thanks && <p>{thanks}</p>}
+          </div>
         </form>
-        {thanks && <p>{thanks}</p>}
       </div>
     </>
   )
