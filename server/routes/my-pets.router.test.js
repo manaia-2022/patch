99eaa8a567f866d3429d98c45e3cc09127b733c7
craig-mclean <107093540+craig-mclean@users.noreply.github.com>
2022-09-15 16:23:31 +1200

    router test setting up

--- server/routes/my-pets.router.test.js
+++ server/routes/my-pets.router.test.js
@@ -12,5 +12,42 @@ beforeAll(async () => {
 vi.mock('../db/functions/getMyPets.db.js')
 
 describe('GET /api/v1/my-pets', () => {
-  it.todo('returns an array of pet objects')
+  it('returns an array of pet objects', () => {
+    db.getMyPets.mockReturnValue(
+      Promise.resolve([
+        {
+          id: 1,
+          ownerId: 'auth0|123456789',
+          name: 'Bella',
+          age: 6,
+          animal: 'dog',
+          bio: 'Bella is a sweet dog',
+          patchPoints: 11987,
+          scratchPoints: 7632,
+          impressions: 21983,
+          createdAt: '2021-01-01T00:00:00.000Z',
+          updatedAt: '2021-01-01T00:00:00.000Z',
+        },
+        {
+          id: 2,
+          ownerId: 'auth0|123456789',
+          name: 'Mable',
+          age: 5,
+          animal: 'cat',
+          bio: 'Mabel is a sweet cat',
+          patchPoints: 11987,
+          scratchPoints: 7632,
+          impressions: 21983,
+          createdAt: '2021-01-01T00:00:00.000Z',
+          updatedAt: '2021-01-01T00:00:00.000Z',
+        },
+      ])
+    )
+    return request(server)
+      .get('/api/v1/my-pets')
+      .then((res) => {
+        console.log(res.body)
+        expect(res.body.name).toContain('roger')
+      })
+  })
 })
