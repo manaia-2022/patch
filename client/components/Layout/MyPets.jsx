4c0c8ea55e807b1b18abdead315c1900af93d32c
craig-mclean <107093540+craig-mclean@users.noreply.github.com>
2022-09-15 12:02:57 +1200

    writing out thunky actions for fetch pets

--- client/components/Layout/MyPets.jsx
+++ client/components/Layout/MyPets.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { getMyPets } from '../../apiClient/my-pets.api'
+import { fetchPets } from '../redux/actions/pets.js'
 
 export default function MyPets() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchPets())
+  }, [])
+
   return <h1>Our Pet Page</h1>
 }
