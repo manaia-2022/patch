2f81df2ae9e57750f4386e8b13715faa1357c853
edan-mourie <107600822+edanmourie@users.noreply.github.com>
2022-09-16 12:35:41 +1200

    voting button styling

--- client/components/Voting.jsx
+++ client/components/Voting.jsx
@@ -3,29 +3,32 @@ import React from 'react'
 import { patchPet, scratchPet } from '../apiClient/apiClient'
 
 export default function Voting(props) {
-  // const { id } = useParams()
   const { petId } = props
   function scratch() {
     scratchPet(petId)
+    // TODO: reload/get another random pet
   }
   function retry() {
-    // reload/get another random pet
+    // TODO: reload/get another random pet
   }
   function patch() {
     patchPet(petId)
+    // TODO: reload/get another random pet
   }
 
   return (
     <>
-      <div>
-        <button onClick={scratch}>Scratch</button>
-      </div>
-      <div>
-        <button onClick={retry}>Retry</button>
-      </div>
-      <div>
-        <button onClick={patch}>Patch</button>
-      </div>
+      <ul className='mt-4 flex flex-col justify-center p-2 md:mt-0 md:flex-row md:space-x-20 md:text-sm md:font-medium'>
+        <li className='w-28 rounded-full border-4 border-red-900 bg-red-300 p-2 text-center text-lg text-red-900 hover:border-red-400 hover:bg-red-900 hover:text-red-400'>
+          <button onClick={scratch}>Scratch</button>
+        </li>
+        <li className='w-28 rounded-full border-4 border-purple-900 bg-purple-300 p-2 text-center text-lg text-purple-900 hover:border-purple-400 hover:bg-purple-900 hover:text-purple-400'>
+          <button onClick={retry}>Retry</button>
+        </li>
+        <li className='w-28 rounded-full border-4 border-green-900 bg-green-300 p-2 text-center text-lg text-green-900 hover:border-green-400 hover:bg-green-900 hover:text-green-400'>
+          <button onClick={patch}>Patch</button>
+        </li>
+      </ul>
     </>
   )
 }
