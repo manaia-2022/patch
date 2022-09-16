6b73a2dc6e0c5cab15040c7137b2933cae863c98
ling_zhang <46408000+ling-starts-code@users.noreply.github.com>
2022-09-16 16:15:11 +1200

    thunk testing, I hope you are happy with this, JV!!

--- client/redux/actions/pets.test.js
+++ client/redux/actions/pets.test.js
@@ -1,6 +1,7 @@
-import nock from 'nock'
-
+import { getRandomPet } from '../../apiClient/pets.api.js'
 import * as actions from './pets.js'
+
+vi.mock('../../apiClient/pets.api.js')
 
 beforeEach(() => {
   vi.clearAllMocks()
@@ -9,10 +10,23 @@ beforeEach(() => {
 describe('testing fetchRandomPet thunk', () => {
   const dispatch = vi.fn()
   it('gets a random pet', () => {
+    getRandomPet.mockReturnValue(Promise.resolve({ name: 'Bella', age: 2 }))
     return actions
-      .fetchRandomPet('Bella')(dispatch)
+      .fetchRandomPet()(dispatch)
       .then(() => {
         expect(dispatch.mock.calls).toHaveLength(2)
+        expect(dispatch.mock.calls[0][0].type).toBe('GET_RANDOM_PET_REQUEST')
+        expect(dispatch.mock.calls[1][0].type).toBe('GET_RANDOM_PET_SUCCESS')
+      })
+  })
+  it('show errors if dispatch fetchRandomPetRequest failed', () => {
+    getRandomPet.mockImplementation(() => Promise.reject(new Error('sad')))
+    return actions
+      .fetchRandomPet()(dispatch)
+      .then(() => {
+        console.log(dispatch.mock.calls[1][0])
+        expect(dispatch.mock.calls[1][0].type).toBe('GET_RANDOM_PET_FAILURE')
+        expect(dispatch.mock.calls[1][0].payload.errMessage).toBe('sad')
       })
   })
 })
